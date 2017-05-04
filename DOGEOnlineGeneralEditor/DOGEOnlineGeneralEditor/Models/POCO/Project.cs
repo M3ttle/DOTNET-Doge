0130fe6f8a7ce7fb98e34bc561fcdfd40af7d280
Arnar Þórðarson <arnart08@ru.is>
2017-05-04 11:50:50 +0000

    POCOs restored

---
+++ DOGEOnlineGeneralEditor/DOGEOnlineGeneralEditor/Models/POCO/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DOGEOnlineGeneralEditor.Models.POCO
+{
+    public class Project
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int FileCount { get; set; }
+        [Column(TypeName = "DateTime")]
+        public DateTime DateCreated { get; set; }
+        public virtual ApplicationUser Owner { get; set; }
+        public bool IsPublic { get; set; }
+        public int LanguageTypeID { get; set; }
+        public virtual LanguageType LanguageType { get; set; }
+        public virtual List<File> Files { get; set; }
+        public virtual List<UserProject> UserProject { get; set; }
+        
+    }
+}
