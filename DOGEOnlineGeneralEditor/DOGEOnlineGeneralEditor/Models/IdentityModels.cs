e36ff4a661eea5022feee8cb9a581e54743dd82a
Arnar Þórðarson <arnart08@ru.is>
2017-05-04 13:00:16 +0000

    fixing my branch

---
+++ DOGEOnlineGeneralEditor/DOGEOnlineGeneralEditor/Models/IdentityModels.cs
@@ -0,0 +1,52 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using DOGEOnlineGeneralEditor.Models.POCO;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace DOGEOnlineGeneralEditor.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        { 
+            
+        }
+
+        
+        public DbSet<Project> Project { get; set; }
+        public DbSet<UserProject> UserProject { get; set; }
+        public DbSet<UserType> UserType { get; set; }
+     
+        public DbSet<LanguageType> LanguageType { get; set; }
+        public DbSet<File> File { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityUserLogin>().HasKey<string>(l => l.UserId);
+            modelBuilder.Entity<IdentityRole>().HasKey<string>(r => r.Id);
+            modelBuilder.Entity<IdentityUserRole>().HasKey(r => new { r.RoleId, r.UserId });
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
+
+            public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ DOGEOnlineGeneralEditor/DOGEOnlineGeneralEditor/Models/POCO/LanguageType.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+namespace DOGEOnlineGeneralEditor.Models.POCO
+{
+    public class LanguageType
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual List<File> Files { get; set; }
+        public virtual List<Project> Projects { get; set; }
+    }
+}
