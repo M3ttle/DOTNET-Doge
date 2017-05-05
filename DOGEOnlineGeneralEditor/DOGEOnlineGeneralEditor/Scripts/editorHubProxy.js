02eda4cfe32f8b77b140cc28281c7361067701db
Nonni <nonni@nocando.is>
2017-05-05 15:57:11 +0000

    Getting up to date

--- DOGEOnlineGeneralEditor/DOGEOnlineGeneralEditor/Scripts/editorHubProxy.js
+++ DOGEOnlineGeneralEditor/DOGEOnlineGeneralEditor/Scripts/editorHubProxy.js
@@ -1,31 +1,48 @@
 ﻿
 var broadcastChanges = function (editor, signal)
 {
-    //console.log($('#saveTextArea').text(editor.getSession().getValue()));
-    // console.log("Pos:");
-    /*console.log("column: " + e.start.column);
-    console.log("row: " + e.start.row);
-    console.log();
-    console.log("Value: " + e.lines);
-    console.log("action: " + e.action); //if action == insert, remove
-    console.log(e);
-    */
-    var row = signal.start.row;
-    var column = signal.start.column;
-    var text = signal.lines;
+    /*
+    var currPos = editor.getCursorPosition();
+    editor.session.insert(currPos, signal);
 
     console.log("Row: " + row);
     console.log("Column: " + column);
     console.log("Text: " + text);
+    */
     //{ row, column }
-    editor.insert(editor.getCursorPosition(), "blabla");
+    //editor.insert(editor.getCursorPosition(), "blabla");
 }
 
 $(function () {
+    //Enable logging
+    $.connection.hub.logging = true;
+    // Create a proxy to the file hub
+    var fileProxy = $.connection.fileHub;
     // Create the editor
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/javascript");
+
+    //row, text, groupID
+    fileProxy.client.updateFile = function (editor, groupID) {
+
+    }
+
+    // Start the connection.
+    // $.connection.hub.
+    $.connection.hub.start().done(function () {
+        console.log("started...");
+        /*
+        $('#sendmessage').click(function () {
+            // Call the Send method on the hub.
+            chat.server.send($('#displayname').val(), $('#message').val());
+            // Clear text box and reset focus for next comment.
+            $('#message').val('').focus();
+        });
+        */
+
+
+    });
 
     // Get text from the editor when save is pressed
     $('#save').on('click', function (e) {
@@ -50,25 +67,37 @@ $(function () {
     });
 
     $('#savefile').on('click', function (e) {
+
         return false;
     });
 
     editor.getSession().on('change', function (e) {
-        broadcastChanges(editor, e);
-    });
+        var activeLine = e.start.row;
 
+        if ($("#editor").keyup(function (){ // To make sure we only take changes when key is pressed space/enter
+            if (e.action == "insert") {
+                //broadcastChanges(editor, e);
+                var lineNumber = e.start.row;
+                var currPos = editor.getCursorPosition();
+                var customPos = { row: lineNumber, column: 0 };
+                var currentLine = e.getSelectionRange;
+                /*
+                while (activeLine < (e.end.row + 1)) {
+                    editor.session.removeGutterDecoration(activeLine, )
+                }
+                */
+                editor.session.insert(customPos, e.lines[0]);
+
+
+            }
+            else if (e.action == "remove") // TODO
+            {
+
+            }
+        }));
+
+        console.log(e);
+    });
+        
 
 });
-
-    /*
-    console.log("Pos:");
-    console.log("column: " + e.start.column);
-    console.log("row: " + e.start.row);
-    console.log();
-    console.log("Value: " + e.lines);
-    console.log("action: " + e.action); //if action == insert, remove
-    */
-
-
-// 1 , 2 , 3, 
-//Fá cursor pos, hverju var bætt við, group ID, 
