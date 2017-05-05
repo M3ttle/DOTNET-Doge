dfad7e3f89b645888dd0d9fd6e38cb225297061f
Arnar Þórðarson <arnart08@ru.is>
2017-05-05 08:25:17 +0000

    added some functions to generalservices

--- DOGEOnlineGeneralEditor/DOGEOnlineGeneralEditor/Services/GeneralService.cs
+++ DOGEOnlineGeneralEditor/DOGEOnlineGeneralEditor/Services/GeneralService.cs
@@ -25,9 +25,48 @@ namespace DOGEOnlineGeneralEditor.Services
                           select x).SingleOrDefault();
             return result;
         }
-        #endregion
 
+        /// <summary>
+        /// Function that returns true if the project with the given projectID contains a file
+        /// whose filename is fileName.
+        /// </summary>
+        /// <param name="projectID"></param>
+        /// <param name="fileName"></param>
+        /// <returns>bool</returns>
+        public bool fileExists(int projectID, string fileName)
+        {
+            File file = (from x in database.File
+                         where x.Name == fileName
+                         && x.Project.ID == projectID
+                         select x).SingleOrDefault();
+            if (file != null)
+            {
+                return true;
+            }
+            return false;
+        }
+        #endregion
+        /// <summary>
+        /// Function that returns true if a user with the given Id has access to a project
+        /// whose name is projectName
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="projectName"></param>
+        /// <returns></returns>
         #region ProjectService
+        public bool projectExists(int userID, string projectName)
+        {
+            Project project = (from x in database.UserProject
+                               where x.Project.Name == projectName
+                               && x.UserID == userID
+                               select x.Project).SingleOrDefault();
+            if(project != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
         #endregion
         #region UserService
         /// <summary>
@@ -51,11 +90,12 @@ namespace DOGEOnlineGeneralEditor.Services
                 UserName = user.Name
             };
         }
+
         /// <summary>
         /// Function that adds a user to the database 
         /// </summary>
         /// <param name="applicationUser"></param>
-        /// <returns></returns>
+        /// <returns>bool</returns>
         bool AddUser(ApplicationUser applicationUser)
         {
             User user = new User
@@ -65,7 +105,28 @@ namespace DOGEOnlineGeneralEditor.Services
                 //ná í UserType og Gender
             };
             database.User.Add(user);
-            database.SaveChanges();
+
+            if(database.SaveChanges() == 1);
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Function that returns true if a User with a given username exists in the database.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>bool</returns>
+        public bool userExists(string name)
+        {
+            User user = (from x in database.User
+                         where x.Name == name
+                         select x).SingleOrDefault();
+            if(user != null)
+            {
+                return true;
+            }
             return false;
         }
         #endregion
