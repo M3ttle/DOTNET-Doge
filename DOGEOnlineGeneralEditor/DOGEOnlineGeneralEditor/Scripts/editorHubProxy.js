c6f2fa1191e175481594965de897e20601139389
Nonni <nonni@nocando.is>
2017-05-05 17:05:28 +0000

    extra jquery included removed

--- DOGEOnlineGeneralEditor/DOGEOnlineGeneralEditor/Scripts/editorHubProxy.js
+++ DOGEOnlineGeneralEditor/DOGEOnlineGeneralEditor/Scripts/editorHubProxy.js
@@ -15,7 +15,7 @@ var broadcastChanges = function (editor, signal)
 
 $(function () {
     //Enable logging
-    //$.connection.hub.logging = true; here
+    //$.connection.hub.logging = true;
 
     // Create a proxy to the file hub
     //var fileProxy = $.connection.fileHub; here
